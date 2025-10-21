0f750860234efc8d4bfb6574507cdfc21967a3b7
ythakar <yk915929@dal.ca>
2025-10-21 14:35:48 -0300

    fix: CORS should reject instead of throwing error

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,9 @@ app.use(cors({
             return callback(null, true);
         }
         
-        callback(new Error('Not allowed by CORS'));
+        // Reject by returning false instead of error
+        console.log('CORS rejected origin:', origin);
+        callback(null, false);
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
